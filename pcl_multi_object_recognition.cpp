e7a128d931de5dbab218064535accaef5ff170c1
Andrew Hundt <ATHundt@gmail.com>
2015-06-20 19:31:03 -0400

    Initial integration of freenect_cloud_grabber. No longer supports recording/playback which will need to be re-added.

--- pcl_multi_object_recognition.cpp
+++ pcl_multi_object_recognition.cpp
@@ -1,8 +1,8 @@
 #include <thread>
 
 #include "find_function.h"
-#include "OpenniStreamer.h"
 #include "Visualizer.h"
+#include "freenect_grabber.hpp"
 
 
 
@@ -25,27 +25,30 @@ main (int argc, char** argv)
     return 1;
   }
 
-  // Check if an oni file is specified as input or if the camera stream has to be used and initialize the correct stream.
-  OpenniStreamer * openni_streamer;
-  if(!oni_file.empty())
-    openni_streamer = new OpenniStreamer(oni_file);
-  else
-    openni_streamer = new OpenniStreamer();
-  // Initialize all the fundamental datastructures
+  boost::shared_ptr<pcl::PointCloud<pcl::PointXYZRGB>> cloud;
+
+  freenectGrabber<pcl::PointXYZRGB> c;
+
+  cloud = c.get_point_cloud(distance, true);
+
+
+  cloud->sensor_orientation_.w () = 0.0;
+  cloud->sensor_orientation_.x () = 1.0;
+  cloud->sensor_orientation_.y () = 0.0;
+  cloud->sensor_orientation_.z () = 0.0;
+  
   int num_threads = model_list.size();
   Visualizer visualizer;
   Semaphore s(num_threads);
   std::vector<std::thread> thread_list(num_threads);
   std::vector<ClusterType> found_models(num_threads);
   ErrorWriter e;
-  int frame_index;
+  int frame_index = 0;;
 
-  // Read first frame and launch the threads
-  if(!openni_streamer->HasDataLeft())
-      exit(0);
-  scene = openni_streamer->GetCloud();
-  copyPointCloud (*scene, *complete_scene);
-  frame_index = openni_streamer->GetFrameIndex();
+  /// @todo support recording and playback of data
+  cloud = c.get_point_cloud(distance, true);
+  frame_index++;
+  copyPointCloud (*cloud, *complete_scene);
 
   for(int i = 0; i < num_threads; ++i)
       thread_list[i] = std::thread(FindObject, model_list[i], std::ref(scene), std::ref(s), std::ref(found_models), i, filters->at(i), icp_iterations->at(i), std::ref(e), std::ref(frame_index));
@@ -64,12 +67,8 @@ main (int argc, char** argv)
     SetViewPoint (complete_scene);
     visualizer.Visualize (model_list, found_models, complete_scene);
     
-    // Grab a frame and create the pointcloud checking in case if the oni stream is finished
-    if(!openni_streamer->HasDataLeft())
-      break;
-    scene = openni_streamer->GetCloud();
-    frame_index = openni_streamer->GetFrameIndex();
-    copyPointCloud (*scene, *complete_scene);
+    copyPointCloud (*cloud, *complete_scene);
+    frame_index++;
     // Wake up the threads
     s.Notify2threads();
   }
